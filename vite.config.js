41f07262ab7556c46b4a7cea4d8d13d40b40d630
saiteja00743 <gajavellisaiteja123@gmail.com>
2026-04-12 20:48:52 +0530

    Allow Render host in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ["aurora-women-saftey-qgmt.onrender.com"]
+  },
+  server: {
+    allowedHosts: ["aurora-women-saftey-qgmt.onrender.com"]
+  }
 })
